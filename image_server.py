01d2b4a7b9b7b57bec833207d96b40a6064ae20a
rich-hart <richhohart@gmail.com>
2014-11-22 13:45:49 -0500

    removing web security lines from image server to keep script simple

--- image_server.py
+++ image_server.py
@@ -13,22 +13,9 @@ class images:
     def GET(self,index):
         
         index = int(index)
-        name = retrieve_image_filename_from_db(index)
         
-        ext = name.split(".")[-1] # Gather extension
-
-        cType = {
-            "png":"images/png",
-            "jpg":"images/jpeg",
-            "gif":"images/gif",
-            "ico":"images/x-icon"            }
-
-        if name in os.listdir('images'):  # Security
-            web.header("Content-Type", cType[ext]) # Set the Header
-            data = retrieve_image_data_from_db(index)
-            return data # Notice 'rb' for reading images
-        else:
-            raise web.notfound()
+        data = retrieve_image_data_from_db(index)
+        return data # Notice 'rb' for reading images
 
 if __name__ == "__main__":
     
