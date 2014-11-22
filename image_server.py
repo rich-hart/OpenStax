4223c3db72a1211091f733d0b555b93d1843e610
rich-hart <richhohart@gmail.com>
2014-11-22 11:25:12 -0500

    adding in image_server.py source code example from webpy.github.io/images webpage

---
+++ image_server.py
@@ -0,0 +1,28 @@
+#Source Code http://webpy.github.io/images
+
+import web
+import os
+
+urls = (
+'/images/(.*)', 'images' #this is where the image folder is located....
+)
+
+class images:
+    def GET(self,name):
+        ext = name.split(".")[-1] # Gather extension
+
+        cType = {
+            "png":"images/png",
+            "jpg":"images/jpeg",
+            "gif":"images/gif",
+            "ico":"images/x-icon"            }
+
+        if name in os.listdir('images'):  # Security
+            web.header("Content-Type", cType[ext]) # Set the Header
+            return open('images/%s'%name,"rb").read() # Notice 'rb' for reading images
+        else:
+            raise web.notfound()
+
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.run()
