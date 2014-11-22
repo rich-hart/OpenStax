77667829162efb50e029b60e79cc10b84d857bb6
rich-hart <richhohart@gmail.com>
2014-11-22 15:09:57 -0500

    adding comments

--- build_database.py
+++ build_database.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
 # source code: http://zetcode.com/db/sqlitepythontutorial/
 import sqlite3 as lite
 import sys
 
-image_directory = "images/"
-#image_file_name = "knot.jpg"
-#image_file_name = "Linux_logo.png"
-image_file_name = "partyanimsm.gif"
+image_directory = "images/" #default directory where images are stored
+database = 'test.db'
 
-def readImage():
+'''function for the reading of images'''
+def readImage(image_file_name):
 
     try:
         fin = open(image_directory+image_file_name, "rb")
@@ -29,33 +29,58 @@ def readImage():
 
 
 
-
-
-try:
-    con = lite.connect('test.db')
-    
-    cur = con.cursor()
-    data = readImage()
-    binary = lite.Binary(data)
-    cur.execute("DROP TABLE IF EXISTS Images")
-    cur.execute("CREATE TABLE Images(image_id INTEGER PRIMARY KEY, image BLOB)")
-    cur.execute("INSERT INTO Images(image) VALUES (?)", (binary,) )
-    cur.execute("DROP TABLE IF EXISTS Images_Names")
-    cur.execute("CREATE TABLE Images_Names(image_id INTEGER PRIMARY KEY, File_name TEXT)")
-    cur.execute("INSERT INTO Images_Names(File_name) VALUES (?)", (image_file_name,) )
-
-    con.commit()    
-    
-except lite.Error, e:
-    
-    if con:
-        con.rollback()
+#
+'''function for loading images into a sqlite database'''
+def load_image_into_database(image_file_name):
+    try:
+        con = lite.connect('test.db')
         
-    print "Error %s:" % e.args[0]
-    sys.exit(1)
+        cur = con.cursor()
+        data = readImage(image_file_name)
+        binary = lite.Binary(data)
+        cur.execute("INSERT INTO Images(image) VALUES (?)", (binary,) )
+        cur.execute("DROP TABLE IF EXISTS Images_Names")
+        cur.execute("CREATE TABLE Images_Names(image_id INTEGER PRIMARY KEY, File_name TEXT)")
+        cur.execute("INSERT INTO Images_Names(File_name) VALUES (?)", (image_file_name,) )
     
-finally:
-    
-    if con:
-        con.close()  
+        con.commit()    
+        
+    except lite.Error, e:
+        
+        if con:
+            con.rollback()
+            
+        print "Error %s:" % e.args[0]
+        sys.exit(1)
+        
+    finally:
+        
+        if con:
+            con.close()  
 
+
+if __name__ == "__main__":
+    
+    try:
+        #connect to the database
+        con = lite.connect(database)
+        cur = con.cursor()
+        #destroy and create a new image table
+        cur.execute("DROP TABLE IF EXISTS Images")
+        cur.execute("CREATE TABLE Images(image_id INTEGER PRIMARY KEY, image BLOB)")
+    except lite.Error, e:
+        
+        if con:
+            con.rollback()
+            
+        print "Error %s:" % e.args[0]
+        sys.exit(1)
+        
+    finally:
+        
+        if con:
+            con.close()
+    #load images from directory into the database
+    load_image_into_database("Linux_logo.png")
+    load_image_into_database("partyanimsm.gif")
+    load_image_into_database("knot.jpg")
--- retrieve_image_from_db.py
+++ retrieve_image_from_db.py
@@ -8,14 +8,17 @@ import sys
 
 database = 'test.db'
 
-
+'''function for reading image data from a database'''
 def retrieve_image_data_from_db(retrieve_index):
     try:
         con = lite.connect(database)
+        
         cur = con.cursor()
         
         cur.execute("SELECT image FROM Images WHERE image_id = %d LIMIT 1"%retrieve_index)
+        
         data = cur.fetchone()[0]
+        
         return data
         
     
@@ -23,6 +26,7 @@ def retrieve_image_data_from_db(retrieve_index):
     except lite.Error, e:
         
         print "Error %s:" % e.args[0]
+        
         sys.exit(1)
         
     finally:
